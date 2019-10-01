58449e5c77fab624664b2266b8184d263d1adabd
fepu08 <besenyeiferenc970308@gmail.com>
2019-10-01 18:31:28 +0200

    Kartyak megjelenitese, ertek kiiratas, gombok, szoveg lathatosaganak beallitasa.

--- BlackJack/src/blackjack/GUI.java
+++ BlackJack/src/blackjack/GUI.java
@@ -1,9 +1,8 @@
 package blackjack;
-import java.awt.Color;
-import java.awt.Font;
-import java.awt.Graphics;
-import java.awt.Image;
-import java.awt.Toolkit;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
@@ -13,23 +12,16 @@ import java.io.File;
 import java.io.IOException;
 import java.util.*;
 
-import javax.imageio.ImageIO;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-
-import blackjack.GUI.ActHit;
-import blackjack.GUI.ActNo;
-import blackjack.GUI.ActRules;
-import blackjack.GUI.ActStand;
-import blackjack.GUI.ActYes;
-
-public class GUI extends JFrame{
-	//randomizer for cards
+public class GUI extends JFrame {
+    //randomizer for cards
     Random rnd = new Random();
-	
-	//button color
+
+    //strings
+    String questHitStay = new String("Hit or Stand?");
+    String questPlayMore = new String("Play more?");
+    String winner = new String();
+
+    //button color
     Color colorButton = Color.yellow;
 
     //buttons
@@ -38,13 +30,7 @@ public class GUI extends JFrame{
     JButton bYes = new JButton();
     JButton bNo = new JButton();
     JButton bRules = new JButton();
-    
-    //fonts
-    Font fontQuest = new Font("Times New Roman", Font.BOLD, 40);
-    Font fontButton = new Font("Times New Roman", Font.PLAIN, 25);
-    Font fontLogo = new Font("Times New Roman", Font.BOLD, 80);
 
-    
     //screen resolution
     int sW = (int) Toolkit.getDefaultToolkit().getScreenSize().getWidth();
     int sH = (int) Toolkit.getDefaultToolkit().getScreenSize().getHeight();
@@ -52,10 +38,30 @@ public class GUI extends JFrame{
     //window resolution
     int aW = 1300;
     int aH = 800;
-    
+
+    //card grid position and dimensions
+    int gridX = 50;
+    int gridY = 50;
+    int gridW = 900;
+    int gridH = 400;
+
     //deck
     Deck deck = new Deck();
-    
+
+    //card spacing and dimensions
+    int spacing = 10;
+    int rounding = 10;
+    int tCardW = (int) gridW / 6;
+    int tCardH = (int) gridH / 2;
+    int cardW = tCardW - spacing * 2;
+    int cardH = tCardH - spacing * 2;
+
+    //boolean for stages
+    boolean hit_stay_q = true;
+    boolean dealer_turn = false;
+    boolean play_more_q = false;
+    boolean dHitter = false;
+
     //player and dealer card array
     ArrayList<Card> pCards = new ArrayList<Card>();
     ArrayList<Card> dCards = new ArrayList<Card>();
@@ -65,15 +71,15 @@ public class GUI extends JFrame{
     int pMaxTotal = 0;
     int dMinTotal = 0;
     int dMaxTotal = 0;
-    
-    //boolean for stages
-    boolean hit_stay_q = true;
-    boolean dealer_turn = false;
-    boolean play_more_q = false;
-    boolean dHitter = false;
+
+    //fonts
+    Font fontQuest = new Font("Times New Roman", Font.BOLD, 40);
+    Font fontButton = new Font("Times New Roman", Font.PLAIN, 25);
+    Font fontLogo = new Font("Times New Roman", Font.BOLD, 80);
+
 
     public GUI() throws IOException {
-    	this.setTitle("Blackjack");
+        this.setTitle("Blackjack");
         this.setBounds((sW - aW - 6) / 2, (sH - aH - 29) / 2, aW + 6, aH + 29);
         this.setResizable(false);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -87,7 +93,7 @@ public class GUI extends JFrame{
 
         Click click = new Click();
         this.addMouseListener(click);
-        
+
         //button stuff
 
         ActHit actHit = new ActHit();
@@ -129,7 +135,78 @@ public class GUI extends JFrame{
         bRules.setFont(fontButton);
         bRules.setText("?");
         board.add(bRules);
+
+        int random = rnd.nextInt(52);
+        pCards.add(deck.getCards().get(random));
+        deck.getCards().get(random).used = true;
+        while (deck.getCards().get(random).used == true) {
+            random = rnd.nextInt(52);
+        }
+        dCards.add(deck.getCards().get(random));
+        deck.getCards().get(random).used = true;
+        random = rnd.nextInt(52);
+        while (deck.getCards().get(random).used == true) {
+            random = rnd.nextInt(52);
+        }
+        pCards.add(deck.getCards().get(random));
+        deck.getCards().get(random).used = true;
+        random = rnd.nextInt(52);
+        while (deck.getCards().get(random).used == true) {
+            random = rnd.nextInt(52);
+        }
+        dCards.add(deck.getCards().get(random));
+        deck.getCards().get(random).used = true;
+
     }
+
+    
+    public void refresher() {
+
+        if (hit_stay_q == true) {
+            bHit.setVisible(true);
+            bStand.setVisible(true);
+        } else {
+            bHit.setVisible(false);
+            bStand.setVisible(false);
+        }
+
+        if (dealer_turn == true) {
+            if (dHitter == false) {}
+                
+        }
+
+        if (play_more_q == true) {
+            bYes.setVisible(true);
+            bNo.setVisible(true);
+        } else {
+            bYes.setVisible(false);
+            bNo.setVisible(false);
+        }
+		
+		if ((pMaxTotal == 21 || pMinTotal >= 21) && hit_stay_q == true) {
+			int tempMax = 0;
+			if (pMaxTotal <= 21) {
+				tempMax = pMaxTotal;
+			} else {
+				tempMax = pMinTotal;
+			}
+			hit_stay_q = false;
+			dealer_turn = true;
+		}
+		
+		if ((dMaxTotal == 21 || dMinTotal >= 21) && dealer_turn == true) {
+			int tempMax = 0;
+			if (dMaxTotal <= 21) {
+				tempMax = dMaxTotal;
+			} else {
+				tempMax = dMinTotal;
+			}
+			dealer_turn = false;
+			play_more_q = true;
+		}
+		
+		repaint();
+	}
     public class Board extends JPanel {
 
         public void paintComponent(Graphics g) {
@@ -153,15 +230,62 @@ public class GUI extends JFrame{
             g.drawString("Your hand:", 350, 190);
             g.drawString("Dealer's hand:", 320, 450);
             
-          
+            //questions
+            if (hit_stay_q == true) {
+                g.setColor(Color.black);
+                g.setFont(fontQuest);
+                g.drawString(questHitStay, gridX + gridW + 60, gridY + 90);
+                g.drawString("Total:", gridX + gridW + 60, gridY + 290);
+                if (pMinTotal == pMaxTotal) {
+                    g.drawString(Integer.toString(pMaxTotal), gridX + gridW + 60, gridY + 350);
+                } else if (pMaxTotal <= 21) {
+                    g.drawString(Integer.toString(pMinTotal) + " or " + Integer.toString(pMaxTotal), gridX + gridW + 60, gridY + 350);
+                } else {
+                    g.drawString(Integer.toString(pMinTotal), gridX + gridW + 60, gridY + 350);
+                }
+            } else if (play_more_q == true) {
+                g.setColor(Color.black);
+                g.setFont(fontQuest);
+                g.drawString(questPlayMore, gridX + gridW + 70, gridY + 500);
+                g.setColor(Color.RED);
+                g.drawString(winner, gridX + gridW + 70, gridY + 450);
+                
+            }            
             
-            
-            
-            
+            //player cards
+			int index = 0;
+			for (Card c : pCards) {
+				g.drawImage(c.getCardImage(), 100+index*110, 230, 100, 150, Color.black, null);
+				index++;
+			}
+			//dealer cards
+			g.drawImage(dCards.get(0).cardImage, 100, 500, 100, 150, Color.black, null);
+			if (dealer_turn == true || play_more_q == true) {
+				index=0;
+				for (Card c : dCards) {
+					g.drawImage(c.getCardImage(), 100+index*110, 500, 100, 150, Color.black, null);
+					index++;
+				
+				}
+				g.setColor(Color.black);
+				g.setFont(fontQuest);
+				g.drawString("Your total: ", gridX+gridW+60, gridY+40);
+				if (pMaxTotal <= 21) {
+					g.drawString(Integer.toString(pMaxTotal), gridX+gridW+60, gridY+120);
+				} else {
+					g.drawString(Integer.toString(pMinTotal), gridX+gridW+60, gridY+120);
+				}
+				g.drawString("Dealer's total: ", gridX+gridW+60, gridY+240);
+				if (dMaxTotal <= 21) {
+					g.drawString(Integer.toString(dMaxTotal), gridX+gridW+60, gridY+320);
+				} else {
+					g.drawString(Integer.toString(dMinTotal), gridX+gridW+60, gridY+320);
+				}
 			}
 			
         }
-    
+    }
+
     public class Move implements MouseMotionListener {
 
         @Override
@@ -204,6 +328,7 @@ public class GUI extends JFrame{
         }
 
     }
+
     public class ActYes implements ActionListener {
 
         @Override
@@ -279,6 +404,7 @@ public class GUI extends JFrame{
 
         @Override
         public void actionPerformed(ActionEvent e) {
+            Main.terminator = true;
             dispose();
         }
 
@@ -291,7 +417,7 @@ public class GUI extends JFrame{
         			"A játék célja, hogy a játékos lapjai összértéke minél közelebb legyen a huszonegyhez, de azt ne lépje túl.\r\n" + 
         			"A játékot egy játékos játssza és a gép, aki az osztó. A játékot az nyeri, akinek a lapjainak az értéke nagyobb. Az osztó addig húz lapot, amíg a lapjainak az értéke\r\n" + 
         			"el nem éri legalább a 17-et.\r\n" + "Akkor van az egyik félnek Blackjack-je, ha az elsõ két kapott lap összértéke 21.\r\n" + "Ha valamelyik fél túllépné a 21-et, akkor azt úgy szokás mondani, hogy besokallt(angolul bust).\r\n"+
-        			"Hit - lapkérés\r\n"+"Stand - Megállás", "Help", JOptionPane.PLAIN_MESSAGE);
+        			"Hit - lapkérés\r\n"+"Stand - Megállás\r\n"+"Push - Ha az osztó és a játékos kártyáinak értéke megegyezik, vagy mind a ketten besokalltak. Ilyenkor egyik fél sem nyer.", "Help", JOptionPane.PLAIN_MESSAGE);
         }
 
     }
