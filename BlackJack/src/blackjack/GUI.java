6dd834b4a477ccf53c949f421ce5675c3dfa6a22
99balint1 <juhaszbalint99@gmail.com>
2019-10-01 17:33:10 +0200

    Ablak beállítása, háttér, felirat elhelyezése, mouse listener

--- BlackJack/src/blackjack/GUI.java
+++ BlackJack/src/blackjack/GUI.java
@@ -1,12 +1,20 @@
 package blackjack;
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Image;
 import java.awt.Toolkit;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+import java.io.File;
 import java.io.IOException;
 import java.util.*;
 
+import javax.imageio.ImageIO;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class GUI extends JFrame{
 	//randomizer for cards
@@ -23,7 +31,10 @@ public class GUI extends JFrame{
     JButton bRules = new JButton();
     
     //fonts
+    Font fontQuest = new Font("Times New Roman", Font.BOLD, 40);
     Font fontButton = new Font("Times New Roman", Font.PLAIN, 25);
+    Font fontLogo = new Font("Times New Roman", Font.BOLD, 80);
+
     
     //screen resolution
     int sW = (int) Toolkit.getDefaultToolkit().getScreenSize().getWidth();
@@ -45,5 +56,91 @@ public class GUI extends JFrame{
     int pTotal = 0;
 
     public GUI() throws IOException {
+    	this.setTitle("Blackjack");
+        this.setBounds((sW - aW - 6) / 2, (sH - aH - 29) / 2, aW + 6, aH + 29);
+        this.setResizable(false);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setVisible(true);
+
+        Board board = new Board();
+        this.setContentPane(board);
+        board.setLayout(null);
+        Move move = new Move();
+        this.addMouseMotionListener(move);
+
+        Click click = new Click();
+        this.addMouseListener(click);
+    }
+    public class Board extends JPanel {
+
+        public void paintComponent(Graphics g) {
+            //background
+            Image img = null;
+            try {
+                img = ImageIO.read(new File("background.jpg"));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            g.drawImage(img, 0, 0, null);
+            
+            //blackjack logo
+            g.setColor(Color.YELLOW);
+            g.setFont(fontLogo);
+            g.drawString("Blackjack", 290, 120);
+            
+            //who's hand
+            g.setColor(Color.black);
+            g.setFont(fontQuest);
+            g.drawString("Your hand:", 350, 190);
+            g.drawString("Dealer's hand:", 320, 450);
+            
+            
+            
+            
+			}
+			
+        }
+    
+    public class Move implements MouseMotionListener {
+
+        @Override
+        public void mouseDragged(MouseEvent arg0) {
+
+        }
+
+        @Override
+        public void mouseMoved(MouseEvent arg0) {
+
+        }
+
+    }
+
+    public class Click implements MouseListener {
+
+        @Override
+        public void mouseClicked(MouseEvent arg0) {
+
+        }
+
+        @Override
+        public void mouseEntered(MouseEvent arg0) {
+
+        }
+
+        @Override
+        public void mouseExited(MouseEvent arg0) {
+
+        }
+
+        @Override
+        public void mousePressed(MouseEvent arg0) {
+
+        }
+
+        @Override
+        public void mouseReleased(MouseEvent arg0) {
+
+        }
+
     }
 }
