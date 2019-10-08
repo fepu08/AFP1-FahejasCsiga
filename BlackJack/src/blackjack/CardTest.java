738c84b1295ac1383b8132e04d15e7abfa47cdd5
fepu08 <besenyeiferenc970308@gmail.com>
2019-10-08 10:48:59 +0200

    CardTest.java - getValue()-teszt elvegezve

--- BlackJack/src/blackjack/CardTest.java
+++ BlackJack/src/blackjack/CardTest.java
@@ -1,21 +1,38 @@
 package blackjack;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
 import static org.junit.Assert.*;
 
 public class CardTest {
+    Deck deck = new Deck();
+
     @org.junit.Test
     public void getCardImage() {
+        for (Card card : deck.getCards())
+        {
+            card.getCardImage();
+        }
     }
 
     @org.junit.Test
     public void getValue() {
+        for (Card card : deck.getCards())
+        {
+            System.out.println(card.getValue());
+        }
     }
 
     @org.junit.Test
     public void getFaceName() {
+
     }
 
     @org.junit.Test
     public void getSuit() {
+    }
+
+    public CardTest() throws IOException {
     }
 }
