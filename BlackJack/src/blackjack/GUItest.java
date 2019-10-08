1e256be569ada61181aa4f4098c45db0f232d2a2
fepu08 <besenyeiferenc970308@gmail.com>
2019-10-08 10:19:02 +0200

    GUItest.java letrehozasa

---
+++ BlackJack/src/blackjack/GUItest.java
@@ -0,0 +1,24 @@
+package blackjack;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GUItest {
+
+    @Test
+    public void totalsChecker() {
+    }
+
+    @Test
+    public void setWinner() {
+    }
+
+    @Test
+    public void dealerHitStand() {
+    }
+
+    @Test
+    public void refresher() {
+    }
+}
