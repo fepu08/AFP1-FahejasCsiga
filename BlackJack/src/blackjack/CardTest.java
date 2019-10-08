5e6d3d60cf1885623a6bc12e40f748d303891057
fepu08 <besenyeiferenc970308@gmail.com>
2019-10-08 10:15:42 +0200

    CardTest.java letrehozasa

---
+++ BlackJack/src/blackjack/CardTest.java
@@ -0,0 +1,22 @@
+package blackjack;
+
+import static org.junit.Assert.*;
+
+public class CardTest {
+
+    @org.junit.Test
+    public void getCardImage() {
+    }
+
+    @org.junit.Test
+    public void getValue() {
+    }
+
+    @org.junit.Test
+    public void getFaceName() {
+    }
+
+    @org.junit.Test
+    public void getSuit() {
+    }
+}
