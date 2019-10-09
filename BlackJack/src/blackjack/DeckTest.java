b85645e0a63c89706c126a43ced296ba9151e447
99balint1 <juhaszbalint99@gmail.com>
2019-10-09 10:49:06 +0200

    DeckTest.java - refreshCards() teszt sikeresen elvégezve

--- BlackJack/src/blackjack/DeckTest.java
+++ BlackJack/src/blackjack/DeckTest.java
@@ -2,12 +2,38 @@ package blackjack;
 
 import org.junit.Test;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
 import static org.junit.Assert.*;
 
 public class DeckTest {
+    Deck d = new Deck();
+
+    public DeckTest() throws IOException {
+    }
 
     @Test
     public void refreshCards() {
+        //Deklaralasnal used:false
+        for (int i = 0; i < d.getCards().size(); i++) {
+            System.out.println(String.format("Value: %s | Suit: %s | Face Name: %s | Used: %s",
+                    d.getCards().get(i).getValue(), d.getCards().get(i).getSuit(), d.getCards().get(i).getFaceName(), d.getCards().get(i).used));
+        }
+
+        //Beallitjuk az used-et true-ra
+        for (int i = 0; i < d.getCards().size(); i++) {
+            d.getCards().get(i).used = true;
+            System.out.println(String.format("Value: %s | Suit: %s | Face Name: %s | Used: %s",
+                    d.getCards().get(i).getValue(), d.getCards().get(i).getSuit(), d.getCards().get(i).getFaceName(), d.getCards().get(i).used));
+        }
+
+        //Megnezzuk hogy a refresh atallitja-e az used-et false-ra
+        d.RefreshCards();
+        for (int i = 0; i < d.getCards().size(); i++) {
+            System.out.println(String.format("Value: %s | Suit: %s | Face Name: %s | Used: %s",
+                    d.getCards().get(i).getValue(), d.getCards().get(i).getSuit(), d.getCards().get(i).getFaceName(), d.getCards().get(i).used));
+        }
     }
 
     @Test
